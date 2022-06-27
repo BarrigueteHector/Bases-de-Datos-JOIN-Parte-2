493b8ee4d32b352ebe87405596d1e1570866fb58
Barriguete Hector <50279990+BarrigueteHector@users.noreply.github.com>
2022-06-26 19:56:36 -0500

    Corrección del ejemplo

--- ejemplo.sql
+++ ejemplo.sql
@@ -1,6 +1,6 @@
 SQL> -- Tenemos las siguientes tablas:
 SQL> DESCRIBE CLIENTE;
- Nombre                                                ¿Nulo?   Tipo
+ Nombre                                                Â¿Nulo?   Tipo
  ----------------------------------------------------- -------- ------------------------------------
  CLAVECTE                                              NOT NULL CHAR(4)
  NOMCTE                                                NOT NULL VARCHAR2(40)
@@ -8,14 +8,14 @@ SQL> DESCRIBE CLIENTE;
  TELCTE                                                NOT NULL CHAR(8)
 
 SQL> DESCRIBE SUCURSAL;
- Nombre                                                ¿Nulo?   Tipo
+ Nombre                                                Â¿Nulo?   Tipo
  ----------------------------------------------------- -------- ------------------------------------
  NUMSUC                                                NOT NULL CHAR(2)
  NOMSUC                                                NOT NULL VARCHAR2(15)
  ACTIVO                                                NOT NULL NUMBER(12,2)
 
 SQL> DESCRIBE CREDITO;
- Nombre                                                ¿Nulo?   Tipo
+ Nombre                                                Â¿Nulo?   Tipo
  ----------------------------------------------------- -------- ------------------------------------
  NUMCTA                                                NOT NULL CHAR(4)
  CLAVECTE                                              NOT NULL CHAR(4)
@@ -57,19 +57,21 @@ NUMC CLAV      SALDO FECHACTA NU
 3726 2574      11923 25/12/05 8                                                                     
 4321 1234       7894 02/06/06 1                                                                     
 
+SQL> -- *************** RETO ***************
+SQL> -- Mostrar el nombre y activo de la sucursal de aquellos clientes que vivan en Xoquipa.
 SQL> 
-SQL> -- Vamos a mostrar el nombre de la sucursal y activo de aquellos clientes que vivan en Xoquipa
+SQL> -- *************** EXPLICACION *************** 
+SQL> -- El reto nos dice que mostremos el nombre (SUCURSAL) y activo (SUCURSAL) de la sucursal de los clientes (CLIENTE) que vivan en Xoquipa.
+SQL> -- Debido a que nuestra condicion se encuentra en la CLIENTE, esta sera nuestra tabla de inicio (del SELECT).
+SQL> -- Para acceder al nombre y activo de la sucursal debemos ir a la tabla SUCURSAL, su PK es NUMSUC, la tabla CREDITO cuenta con ese campo.
+SQL> -- Para acceder a CREDITO lo debemos hacer desde CLIENTE, el campo que tienen en comÃºn las tablas es CLAVECTE.
 SQL> 
-SQL> -- El problema nos dice que necesitamos el nombre de la sucursal (SUCURSAL) y su activo (SUCURSAL) de los clientes (CLIENTE) que vivan en Xoquipa.
-SQL> -- Debido a que nuestra condici¢n se encuentra en la CLIENTE, esta sera nuestra tabla de inicio.
-SQL> -- Para acceder al nombre y activo de la sucursal, la PK es NUMSUC, pero la tabla con la que podemos acceder a SUCURSAL es CREDITO.
-SQL> -- Para acceder a CREDITO lo podemos hacer desde CLIENTE.
-SQL> 
-SQL> -- Con lo anterior nos damos cuenta que existe un orden en el que hay que colocar los JOIN, primero va el de CREDITO y despues el de SUCURSAL. De hacerlo al reves, la consulta no serviria.
+SQL> -- Con lo anterior nos damos cuenta que existe un orden en el que hay que colocar los JOIN, primero el de CREDITO y despues el de SUCURSAL. De hacerlo al reves, la consulta no serviria.
 SQL> -- Entonces, veamos como se realizan estas consultas:
 SQL> 
 SQL> -- ************ JOIN USING ************
 SQL> -- En el SELECT no es necesario indicar la tabla de la que provienen las columnas
+SQL> -- En los parentesis del JOIN colocamos el campo que vamos a utilizar para hacer la conexion.
 SQL> SELECT NOMSUC, ACTIVO FROM CLIENTE
   2  JOIN CREDITO USING (CLAVECTE)
   3  JOIN SUCURSAL USING (NUMSUC)
@@ -83,6 +85,7 @@ Insurgentes       50000000
 SQL> 
 SQL> -- ************ JOIN ON ************
 SQL> -- En el SELECT es necesario indicar la tabla de la que provienen las columnas
+SQL> -- En la condicion del JOIN debemos colocar la tabla del SELECT y campo (separados por un punto) igualado con la tabla del JOIN y campo (separados por un punto) para hacer la conexion entre ambas tablas. El orden de las tablas en la condicion no importa.
 SQL> SELECT SUCURSAL.NOMSUC, SUCURSAL.ACTIVO FROM CLIENTE
   2  JOIN CREDITO ON CLIENTE.CLAVECTE = CREDITO.CLAVECTE
   3  JOIN SUCURSAL ON CREDITO.NUMSUC = SUCURSAL.NUMSUC
@@ -108,4 +111,3 @@ Insurgentes       50000000
 
 SQL> 
 SQL> -- Como vemos, obtenemos el mismo resultado. Depende de nosotros escoger uno y al menos en ORACLE, es posible utilizar estos tres tipos de JOIN.
-SQL> SPOOL OFF;
